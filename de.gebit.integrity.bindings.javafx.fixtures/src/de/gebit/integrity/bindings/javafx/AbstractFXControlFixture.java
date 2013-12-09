76382200d1eaa4704087369f8144cf85bbcded8e
hserfas <hserfas@gebit.de>
2013-12-09 11:38:10 +0100

    added header

--- de.gebit.integrity.bindings.javafx.fixtures/src/de/gebit/integrity/bindings/javafx/AbstractFXControlFixture.java
+++ de.gebit.integrity.bindings.javafx.fixtures/src/de/gebit/integrity/bindings/javafx/AbstractFXControlFixture.java
@@ -8,6 +8,13 @@ import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
+/*******************************************************************************
+ * Copyright (c) 2013 GEBIT Solutions GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 public class AbstractFXControlFixture {
 
     /**
